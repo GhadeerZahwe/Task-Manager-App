892c6b777c16bf60e707225071d208ce31fc6837
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-07 23:52:12 +0200

    start with render tasks function

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
+// Object Array for local storage
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
 const taskManagerContainer = document.querySelector(".taskManager");
 const confirmElement = document.querySelector(".confirm");
-const confirmedBtn = document.querySelector(".confirmed");
-const cancelBtn = document.querySelector(".cancel");
+const confirmedBtn = confirmElement.querySelector(".confirmed");
+const cancelBtn = confirmElement.querySelector(".cancel");
 
-// Global variable initialized with an empty array for local storage
-const tasks= []
 
 // Event Listener for adding a task
-let taskform = document.getElementById("taskForm");
-taskform.addEventListener("submit", handleFormSubmit);
+document.getElementById("taskForm").addEventListener("submit", handleFormSubmit);
 
 // function to handle the form submit event
 function handleFormSubmit(event) {
@@ -17,23 +18,37 @@ function handleFormSubmit(event) {
   const taskInput = document.getElementById("taskInput");
   // trim to print the value without spaces
   const taskValue = taskInput.value.trim();
-  if(taskValue!==" ")
-  {
-    const newTask={
-        text:taskValue,
-        completed:false
+  if (taskValue !== " ") {
+    const newTask = {
+      text: taskValue,
+      completed: false,
     };
     tasks.push(newTask);
     saveTasks();
     // clear after save
-    taskInput.value=" ";
+    taskInput.value = " ";
     // render tasks to show them as cards
     renderTasks();
-
   }
 }
 
 // function to save the tasks
-function saveTasks(){
-    localStorage.setItem(tasks,JSON.stringify(tasks));
+function saveTasks() {
+  localStorage.setItem(tasks, JSON.stringify(tasks));
+}
+
+// Initial rendering of tasks
+renderTasks();
+
+// function to render the tasks
+function renderTasks(){
+    const taskContainer= document.getElementById("taskContainer");
+    taskContainer.innerHTML = " ";
+    tasks.forEach((task,index) => {
+    const taskCard = document.createElement("div");
+    taskCard.classList.add("taskCard");
+    let classVal = "pending"
+    let statusVal="Pending"
+    
+  });
 }
