e5fc4bf257c2ae6fea9b5668d525b0fe425fb603
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-07 22:38:56 +0200

    function to handle the submit form

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const taskManagerContainer = document.querySelector(".taskManager");
-const confirmElement= document.querySelector(".confirm");
-const confirmedBtn= document.querySelector(".confirmed");
-const cancelBtn= document.querySelector(".cancel");
+const confirmElement = document.querySelector(".confirm");
+const confirmedBtn = document.querySelector(".confirmed");
+const cancelBtn = document.querySelector(".cancel");
+
+// Global variable initialized with an empty array for local storage
+const tasks= []
 
 // Event Listener for adding a task
-let taskform= document.getElementById("taskForm");
-taskform.addEventListener("submit",handleFormSubmit)
+let taskform = document.getElementById("taskForm");
+taskform.addEventListener("submit", handleFormSubmit);
+
+// function to handle the form submit event
+function handleFormSubmit(event) {
+  // prevent default behaviour of submit form
+  event.preventDefault();
+  const taskInput = document.getElementById("taskInput");
+  // trim to print the value without spaces
+  const taskValue = taskInput.value.trim();
+  if(taskValue!==" ")
+  {
+    const newTask={
+        text:taskValue,
+        completed:false
+    };
+    tasks.push(newTask);
+    // saveTasks();
+  }
+}
