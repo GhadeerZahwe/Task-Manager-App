57402ceed96e23291f687b80322e304fbc8df0cd
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-09 15:17:45 +0200

    add filter function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Object Array for local storage
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-let indexToBeDeleted= null;
+let indexToBeDeleted = null;
 
 const taskManagerContainer = document.querySelector(".taskManager");
 const todoList = document.querySelector(".taskContainer");
@@ -13,32 +13,32 @@ document.getElementById("taskForm").addEventListener("submit", handleFormSubmit)
 
 // function to handle the form submit event
 function handleFormSubmit(event) {
-    // prevent default behaviour of submit form
-    event.preventDefault();
-    const taskInput = document.getElementById("taskInput");
-    // trim to print the value without spaces
-    const taskValue = taskInput.value.trim();
-    if (taskValue !== "") { // Check if the task value is not empty
-      const newTask = {
-        text: taskValue,
-        completed: false
-      };
-      tasks.push(newTask);
-      saveTasks();
-      // clear after save
-      taskInput.value = "";
-      // render tasks to show them as cards
-      renderTasks();
-    } else {
-      // Handle the case when the task value is empty
-      alert("Please enter a non-empty task.");
-    }
+  // prevent default behaviour of submit form
+  event.preventDefault();
+  const taskInput = document.getElementById("taskInput");
+  // trim to print the value without spaces
+  const taskValue = taskInput.value.trim();
+  if (taskValue !== "") {
+    // Check if the task value is not empty
+    const newTask = {
+      text: taskValue,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    // clear after save
+    taskInput.value = "";
+    // render tasks to show them as cards
+    renderTasks();
+  } else {
+    // Handle the case when the task value is empty
+    alert("Please enter a non-empty task.");
   }
-  
+}
 
 // function to save the tasks
 function saveTasks() {
-  localStorage.setItem(tasks, JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Initial rendering of tasks
@@ -46,7 +46,6 @@ renderTasks();
 
 // function to render the tasks
 function renderTasks() {
-
   const taskContainer = document.getElementById("taskContainer");
   taskContainer.innerHTML = " ";
   tasks.forEach((task, index) => {
@@ -76,11 +75,10 @@ function renderTasks() {
     btnContentElement.classList.add("green");
     toggleButton.appendChild(btnContentElement);
     toggleButton.addEventListener("click", () => {
-    tasks[index].completed= !tasks[index].completed;
-    saveTasks();
-    renderTasks();
-    })
-
+      tasks[index].completed = !tasks[index].completed;
+      saveTasks();
+      renderTasks();
+    });
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("button-box");
@@ -88,11 +86,11 @@ function renderTasks() {
     delBtnContentElement.innerText = "Delete";
     delBtnContentElement.classList.add("red");
     deleteButton.appendChild(delBtnContentElement);
-    deleteButton.addEventListener("click",() => {
-        indexToBeDeleted=index;
-        confirmElement.style.display="block";
-        taskManagerContainer.classList.add("overlay");
-    })
+    deleteButton.addEventListener("click", () => {
+      indexToBeDeleted = index;
+      confirmElement.style.display = "block";
+      taskManagerContainer.classList.add("overlay");
+    });
 
     const editButton = document.createElement("button");
     editButton.classList.add("button-box");
@@ -114,58 +112,67 @@ function renderTasks() {
 }
 
 // Confirm Button EventListener
-confirmedBtn.addEventListener("click", ()=>{
-    confirmElement.style.display="none";
-    taskManagerContainer.classList.remove("overlay");
-    deleteTask(indexToBeDeleted);
-})
+confirmedBtn.addEventListener("click", () => {
+  confirmElement.style.display = "none";
+  taskManagerContainer.classList.remove("overlay");
+  deleteTask(indexToBeDeleted);
+});
 
 // Cancel Button EventListener
-cancelBtn.addEventListener("click", ()=>{
-    confirmElement.style.display="none";
-    taskManagerContainer.classList.remove("overlay");
-})
+cancelBtn.addEventListener("click", () => {
+  confirmElement.style.display = "none";
+  taskManagerContainer.classList.remove("overlay");
+});
 
 // Function to delete task
-function deleteTask(index){
-    tasks.splice(index,1);
-    saveTasks();
-    renderTasks();
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks();
+  renderTasks();
 }
 
 // Edit Button Event Listener
 function editTask(index) {
-  const editModal = document.getElementById('editModal');
-  const editTaskInput = document.getElementById('editTaskInput');
-  editModal.style.display = 'block';
+  const editModal = document.getElementById("editModal");
+  const editTaskInput = document.getElementById("editTaskInput");
+  editModal.style.display = "block";
   editTaskInput.value = tasks[index].text;
 
-  const editSaveBtn = document.getElementById('editSaveBtn');
-  const editCancelBtn = document.getElementById('editCancelBtn');
+  const editSaveBtn = document.getElementById("editSaveBtn");
+  const editCancelBtn = document.getElementById("editCancelBtn");
 
-  editSaveBtn.addEventListener('click', () => {
-    if (editTaskInput.value.trim() !== '') {
+  editSaveBtn.addEventListener("click", () => {
+    if (editTaskInput.value.trim() !== "") {
       tasks[index].text = editTaskInput.value.trim();
       saveTasks();
       renderTasks();
-      editModal.style.display = 'none';
+      editModal.style.display = "none";
     } else {
-      alert('Please enter a non-empty task.');
+      alert("Please enter a non-empty task.");
     }
   });
 
-  editCancelBtn.addEventListener('click', () => {
-    editModal.style.display = 'none';
+  editCancelBtn.addEventListener("click", () => {
+    editModal.style.display = "none";
   });
 }
 
+// filter tasks
+const completedTasks = document.getElementById("Completed");
+completedTasks.addEventListener("click", () => {
+  const filteredTasks = tasks.filter((task) => task.completed);
+  renderFilteredTasks(filteredTasks);
+});
 
-// function filterTasksByStatus(status) {
-//   if (status === "all") {
-//       return tasks;
-//   } else if (status === "completed") {
-//       return tasks.filter(task => task.completed);
-//   } else if (status === "incomplete") {
-//       return tasks.filter(task => !task.completed);
-//   }
-// }
+const activeTasks = document.getElementById("active");
+activeTasks.addEventListener("click", () => {
+  const filteredTasks = tasks.filter((task) => !task.completed);
+  renderFilteredTasks(filteredTasks);
+});
+
+
+const all = document.getElementById("allTasks");
+all.addEventListener("click", () => {
+  renderTasks();
+});
+
